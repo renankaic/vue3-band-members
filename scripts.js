21f263734de4227e6b46e00037cb2d2609097e2a
Renan Kaic <renan-kaic@outlook.com>
2021-10-03 23:01:56 -0300

    Feat - Adding simple form validation

--- scripts.js
+++ scripts.js
@@ -21,8 +21,12 @@ const handlingForms = {
     },
     methods: {
         addMember() {
-            this.members.push(this.newMember)
-            this.newMember = {}
+            if (this.newMember.fname && this.newMember.lname && this.newMember.instrument) {
+                this.members.push(this.newMember)
+                this.newMember = {}
+            } else {
+                alert('All fields must be filled')
+            }
         }
     }
 
